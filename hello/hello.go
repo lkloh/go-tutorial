2c2c92132a91a5dc54248c70911f44c226952339
LayKuan Loh <lkloh2410@gmail.com>
2022-07-19 18:43:37 -0700

    random greeting

--- hello/hello.go
+++ hello/hello.go
@@ -1,13 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "rsc.io/quote"
-    "example.com/greetings"
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+	"rsc.io/quote"
 )
 
 func main() {
-    fmt.Println(quote.Go())
-    message := greetings.Hello("Gladys")
-    fmt.Println(message)
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	fmt.Println(quote.Go())
+
+	message, err := greetings.Hello("Gladys")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
+
+	message2, err2 := greetings.Hello("")
+	if err2 != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message2)
+
 }
